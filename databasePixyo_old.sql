cd38356db96626c0ee937475f2e5b2dfce5263ba
DESKTOP-UC4551N\pablo <trabajo.pablopez@gmail.com>
2021-09-15 14:44:12 +0200

    Updated DB

--- databasePixyo_old.sql
+++ databasePixyo_old.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         10.4.20-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             11.0.0.5919
+-- Server version:               10.4.13-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -12,11 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Volcando estructura de base de datos para pixyo
+-- Dumping database structure for pixyo
 CREATE DATABASE IF NOT EXISTS `pixyo` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `pixyo`;
 
--- Volcando estructura para tabla pixyo.assistants
+-- Dumping structure for table pixyo.assistants
 CREATE TABLE IF NOT EXISTS `assistants` (
   `EventId` int(11) DEFAULT NULL,
   `UserId` int(11) DEFAULT NULL,
@@ -26,9 +26,11 @@ CREATE TABLE IF NOT EXISTS `assistants` (
   CONSTRAINT `event` FOREIGN KEY (`EventId`) REFERENCES `event` (`EventId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Dumping data for table pixyo.assistants: ~0 rows (approximately)
+/*!40000 ALTER TABLE `assistants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `assistants` ENABLE KEYS */;
 
--- Volcando estructura para tabla pixyo.event
+-- Dumping structure for table pixyo.event
 CREATE TABLE IF NOT EXISTS `event` (
   `EventId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) DEFAULT NULL,
@@ -41,33 +43,24 @@ CREATE TABLE IF NOT EXISTS `event` (
   PRIMARY KEY (`EventId`),
   KEY `CreatorId` (`CreatorId`),
   CONSTRAINT `CreatorUser` FOREIGN KEY (`CreatorId`) REFERENCES `user` (`UserId`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
-
--- La exportación de datos fue deseleccionada.
-
--- Volcando estructura para tabla pixyo.event_pics
-CREATE TABLE IF NOT EXISTS `event_pics` (
-  `PicId` int(11) NOT NULL AUTO_INCREMENT,
-  `EventId` int(11) NOT NULL DEFAULT 0,
-  `Pic` longblob NOT NULL DEFAULT '0',
-  `scanned` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`PicId`),
-  KEY `EventId` (`EventId`),
-  CONSTRAINT `EventId` FOREIGN KEY (`EventId`) REFERENCES `event` (`EventId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Dumping data for table pixyo.event: ~0 rows (approximately)
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
 
--- Volcando estructura para tabla pixyo.payment_method
+-- Dumping structure for table pixyo.payment_method
 CREATE TABLE IF NOT EXISTS `payment_method` (
   `MethodId` int(11) NOT NULL AUTO_INCREMENT,
   `Description` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`MethodId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Dumping data for table pixyo.payment_method: ~0 rows (approximately)
+/*!40000 ALTER TABLE `payment_method` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment_method` ENABLE KEYS */;
 
--- Volcando estructura para tabla pixyo.payment_plan
+-- Dumping structure for table pixyo.payment_plan
 CREATE TABLE IF NOT EXISTS `payment_plan` (
   `PlantId` int(11) NOT NULL AUTO_INCREMENT,
   `Description` varchar(50) NOT NULL DEFAULT '0',
@@ -76,41 +69,45 @@ CREATE TABLE IF NOT EXISTS `payment_plan` (
   PRIMARY KEY (`PlantId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Dumping data for table pixyo.payment_plan: ~0 rows (approximately)
+/*!40000 ALTER TABLE `payment_plan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment_plan` ENABLE KEYS */;
 
--- Volcando estructura para tabla pixyo.pic_management
-CREATE TABLE IF NOT EXISTS `pic_management` (
-  `picid` int(11) DEFAULT NULL,
-  `userid` int(11) DEFAULT NULL,
-  `appears` tinyint(4) DEFAULT NULL,
-  `downloaded` tinyint(4) DEFAULT NULL,
-  KEY `FK_PIC` (`picid`),
-  KEY `FK_USER` (`userid`),
-  CONSTRAINT `FK_PIC` FOREIGN KEY (`picid`) REFERENCES `event_pics` (`PicId`),
-  CONSTRAINT `FK_USER` FOREIGN KEY (`userid`) REFERENCES `user` (`UserId`)
+-- Dumping structure for table pixyo.pics
+CREATE TABLE IF NOT EXISTS `pics` (
+  `PicId` int(11) NOT NULL AUTO_INCREMENT,
+  `EventId` int(11) NOT NULL DEFAULT 0,
+  `Pic` blob NOT NULL DEFAULT '0',
+  PRIMARY KEY (`PicId`),
+  KEY `EventId` (`EventId`),
+  CONSTRAINT `EventId` FOREIGN KEY (`EventId`) REFERENCES `event` (`EventId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Dumping data for table pixyo.pics: ~0 rows (approximately)
+/*!40000 ALTER TABLE `pics` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pics` ENABLE KEYS */;
 
--- Volcando estructura para tabla pixyo.user
+-- Dumping structure for table pixyo.user
 CREATE TABLE IF NOT EXISTS `user` (
   `UserId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
-  `Face` longblob DEFAULT NULL,
+  `Face` blob DEFAULT NULL,
   `Phone_Number` varchar(50) DEFAULT NULL,
   `email` text DEFAULT NULL,
-  `Shared_pics` int(11) DEFAULT NULL,
-  `Payment_method` int(11) DEFAULT NULL,
-  `Payment_plan` int(11) DEFAULT NULL,
+  `SharedPics` int(11) DEFAULT NULL,
+  `PaymentMethod` int(11) DEFAULT NULL,
+  `PaymentPlan` int(11) DEFAULT NULL,
   PRIMARY KEY (`UserId`),
-  KEY `PaymentMethod` (`Payment_method`) USING BTREE,
-  KEY `PaymentPlan` (`Payment_plan`) USING BTREE,
-  CONSTRAINT `PaymentMethod` FOREIGN KEY (`Payment_method`) REFERENCES `payment_method` (`MethodId`),
-  CONSTRAINT `PaymentPlan` FOREIGN KEY (`Payment_plan`) REFERENCES `payment_plan` (`PlantId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+  KEY `PaymentMethod` (`PaymentMethod`),
+  KEY `PaymentPlan` (`PaymentPlan`),
+  CONSTRAINT `PaymentMethod` FOREIGN KEY (`PaymentMethod`) REFERENCES `payment_method` (`MethodId`),
+  CONSTRAINT `PaymentPlan` FOREIGN KEY (`PaymentPlan`) REFERENCES `payment_plan` (`PlantId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- La exportación de datos fue deseleccionada.
+-- Dumping data for table pixyo.user: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
